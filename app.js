7680a800bd252eebfb3815bcb4d6f05a8143f45a
Ethnic Sai Arroju <ethnicsai.arroju2003@gmail.com>
2024-11-30 11:15:05 +0530

    updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/studentsDB', {
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.jucku.mongodb.net/studentsDB?retryWrites=true&w=majority&appName=Cluster0', {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
 })
