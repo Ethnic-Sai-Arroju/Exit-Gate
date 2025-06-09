ee0e07945ad79a394089f7413c65c5879b8b7837
Ethnic-Sai-Arroju <ethnicsai.arroju2003@gmail.com>
2025-06-09 22:23:04 +0530

    resolved

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect('process.env.MONGO_URI', {
+mongoose.connect(process.env.MONGO_URI, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
 })
